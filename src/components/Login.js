b117be11cbe43d8b8c0be77d4425f02815383823
mabdalrazaq <m.abdalrazaq314@gmail.com>
2021-03-21 21:28:52 +0200

    debugging problem with infinite reloading

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,30 +3,37 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import LoadingComponent from './LoadingComponent';
 function Login (props){
-    const {isAuthenticated,loginWithRedirect,error,isLoading,getAccessTokenSilently}=useAuth0();
-    if(isAuthenticated){
-        return (
-            <>
-                <h1>You logged in successfully!</h1>
-                <Link to='/home'>Back To App</Link>
-            </>
-        )
-    }
-    else if(isLoading){
-        return <LoadingComponent/>
-    }
-    else if(error){
-        return <h1>can not log in</h1>
-    }
-    else{
+    const {isAuthenticated,loginWithRedirect,error,isLoading,getAccessTokenSilently,user,getAccessTokenWithPopup}=useAuth0();
+    // if(isAuthenticated){
+    //     return (
+    //         <>
+    //             <h1>You logged in successfully!</h1>
+    //             <Link to='/home'>Back To App</Link>
+    //         </>
+    //     )
+    // }
+    // else if(isLoading){
+    //     return <LoadingComponent/>
+    // }
+    // else if(error){
+    //     return <h1>can not log in</h1>
+    // }
+    // else{
         setTimeout(()=>{
-            getAccessTokenSilently().catch((err)=>{
+            getAccessTokenSilently()
+            .then(token=>{console.log(token);alert(token)})
+            .catch(err=>{
                 console.log(err);
-                loginWithRedirect();
-            })
-        },1000)
-        return null;
-    }
+                alert(err.message);
+                loginWithRedirect({
+                    audience:"https://mazing.com",
+                    // redirectUri:"http://localhost:3000/login"
+                    redirectUri="https://mazingame.com/login"
+                });
+            });
+        },500)
+        return <h1>............................</h1>;
+    // }
 }
 
 export default Login;
