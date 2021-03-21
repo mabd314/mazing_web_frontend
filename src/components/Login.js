74b07beaabb7114332c12b950c6f6eb108ac454d
mabdalrazaq <m.abdalrazaq314@gmail.com>
2021-03-21 21:42:28 +0200

    continue

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,42 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import LoadingComponent from './LoadingComponent';
 function Login (props){
-    const {isAuthenticated,loginWithRedirect,error,isLoading,getAccessTokenSilently,user,getAccessTokenWithPopup}=useAuth0();
-    // if(isAuthenticated){
-    //     return (
-    //         <>
-    //             <h1>You logged in successfully!</h1>
-    //             <Link to='/home'>Back To App</Link>
-    //         </>
-    //     )
-    // }
-    // else if(isLoading){
-    //     return <LoadingComponent/>
-    // }
-    // else if(error){
-    //     return <h1>can not log in</h1>
-    // }
-    // else{
-        setTimeout(()=>{
-            getAccessTokenSilently()
-            .then(token=>{console.log(token);alert(token)})
-            .catch(err=>{
-                console.log(err);
-                alert(err.message);
-                loginWithRedirect({
-                    audience:"https://mazing.com",
-                    // redirectUri:"http://localhost:3000/login"
-                    redirectUri:"https://mazingame.com/login"
-                });
-            });
-        },500)
-        return <h1>............................</h1>;
-    // }
+
+    const {isAuthenticated,loginWithRedirect,error,isLoading,getAccessTokenSilently,user}=useAuth0();
+    const {token,setToken}=useState("");
+    useEffect(async () => {
+        try{
+            console.log(user);
+            token=await getAccessTokenSilently();
+            console.log(user);
+        }catch(err){
+            console.log(err);
+            loginWithRedirect();
+        }
+    },[]);
+
+    if(isAuthenticated){
+        return (
+            <>
+                <h1>You logged in successfully!</h1>
+                <Link to='/home'>Back To App</Link>
+            </>
+        )
+    }
+    else if(isLoading){
+        return <LoadingComponent/>
+    }
+    else if(error){
+        return <h1>can not log in</h1>
+    }
+    else if(token==="error"){
+        return <h1>token=error</h1>
+    }
+    else{
+        return <h1>{token}</h1>;
+    }
 }
 
 export default Login;
